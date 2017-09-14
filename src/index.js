4797db133671c0769a26fb1db50e9ae34d469790
Dean Norman <dean@22seven.com>
2017-09-14 11:17:49 +0200

    added glob state and basic scaffolding

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// global state obj
+const state = {
+    todos: [
+        { id: 1, name: 'Item 1', isComplete: true },
+        { id: 2, name: 'Item 2', isComplete: true },
+        { id: 3, name: 'Item 3', isComplete: true },
+    ]
+}
+ReactDOM.render(<App todos={state.todos} />, document.getElementById('root'));
 registerServiceWorker();

e2db3d14dd37d5b407be80e68c8a2d1496cf8185
Dean Norman <dean@22seven.com>
2017-09-19 13:09:04 +0200

    added store

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store'
 
-// global state obj
-const state = {
-    todos: [
-        { id: 1, name: 'Item 1', isComplete: true },
-        { id: 2, name: 'Item 2', isComplete: true },
-        { id: 3, name: 'Item 3', isComplete: true },
-    ]
-}
-ReactDOM.render(<App todos={state.todos} />, document.getElementById('root'));
+const state = store.getState()
+ReactDOM.render(<App {...state} />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/todo.js
@@ -0,0 +1,16 @@
+const initState = {
+    todos: [
+        { id: 1, name: 'create store', isComplete: true },
+        { id: 2, name: 'load state', isComplete: true },
+        { id: 3, name: 'handle state', isComplete: false },
+    ]
+}
+
+export default (state = initState, action) => {
+    switch (action.type) {
+        case 'TODO_ADD':
+            return { ...state, todos: state.todos.concat(action.payload)}
+        default:
+            return state        
+    }
+}
